0ee9ca9dbc341e0064bf19122d4b1c4552aa744e
Vasiadarik <vasiadarik@github.com>
2023-11-12 17:02:25 +0100

    Add project files.

---
+++ Lesson-1/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+namespace Lesson
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string name = "Vasyl";
+
+            string surname = "Darichesku";
+
+            int age = 20;
+
+            Console.WriteLine("name:    " + name);
+
+            Console.WriteLine("surname: " + surname);
+
+            Console.WriteLine("age:     " + age);
+        }
+    }
+}
+
+
